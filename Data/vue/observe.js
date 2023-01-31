2ae9ef7a540c9905c921e09909ce0f341b31aae5
zuowendong <17326195691@163.com>
2023-01-31 22:29:05 +0800

    搭建rollup配置环境

--- Data/vue/observe.js
+++
@@ -1,9 +0,0 @@
-import Observer from "./observer";
-
-function observe(data) {
-    // 观察的不是对象直接退出
-    if (typeof data !== "object" || data === null) return
-    return new Observer(data);
-}
-
-export default observe;
--- README.md
+++
@@ -1,8 +0,0 @@
-# sourceCode-of-VueDemo
-vue源码示例
-
-## Data
-Vue响应式
-
-## DomDiff
-vue dom diff算法基础流程
